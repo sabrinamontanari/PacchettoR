3a0157109084618157ef47600a52099fb2d1cf73
sabrinamontanari <sabrina.montanari4@studio.unibo.it>
2026-04-30 15:01:21 +0200

    Create numero.R

---
+++ numero.R
@@ -0,0 +1,10 @@
+numero <- function(x) {                               #su R: positivo(numero incognito)
+      if(x>0) {
+      "Questo numero è positivo"
+      }
+      else if(x<0){
+      "Questo numero è negativo"}
+      else {                              #oppure else(x=0)
+      "Zero non è positivo nè negativo"
+      }
+}
